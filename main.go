8f365ecfea07c99148d4172623a905dac21b575e
Linus Lee <linus@thesephist.com>
2019-06-22 21:04:43 -0700

    Implement exec-ing files

--- main.go
+++ main.go
@@ -3,35 +3,71 @@ package main
 import (
 	"bufio"
 	"flag"
+	"log"
 	"os"
+	"strings"
 )
 
-// TODO: By default, read from stdin but if passed flag -i --input,
-// 	read from input file(s) including imports if multipel inputs.
+// input files flag parsing
+type inkFiles []string
+
+func (i *inkFiles) Set(val string) error {
+	*i = append(*i, val)
+	return nil
+}
+
+func (i *inkFiles) String() string {
+	return strings.Join(*i, ", ")
+}
+
 func main() {
-	// flags
+	// cli arguments
+	verbose := flag.Bool("verbose", false, "Log all interpreter steps")
 	debugLexer := flag.Bool("debug-lex", false, "Log lexer output")
 	debugParser := flag.Bool("debug-parse", false, "Log parser output")
 
+	var files inkFiles
+	flag.Var(&files, "input", "Source code to execute")
+
 	flag.Parse()
 
+	// rep(l)
 	input := make(chan rune)
 	done := make(chan bool, 3)
 
 	iso := Isolate{}
 	tokens := make(chan Tok)
 	nodes := make(chan Node)
-	go Tokenize(input, tokens, *debugLexer, done)
-	go Parse(tokens, nodes, *debugParser, done)
+	go Tokenize(input, tokens, *debugLexer || *verbose, done)
+	go Parse(tokens, nodes, *debugParser || *verbose, done)
 	go iso.Eval(nodes, done)
 
-	inputReader := bufio.NewReader(os.Stdin)
-	for {
-		char, _, err := inputReader.ReadRune()
-		if err != nil {
-			break
+	if len(files) > 0 {
+		for _, path := range files {
+			file, err := os.Open(path)
+			if err != nil {
+				log.Fatalf("Could not open %s for execution: %s", path, err)
+			}
+			defer file.Close()
+
+			scanner := bufio.NewScanner(file)
+
+			for scanner.Scan() {
+				for _, char := range scanner.Text() {
+					input <- char
+				}
+				input <- '\n'
+			}
 		}
-		input <- char
+	} else {
+		inputReader := bufio.NewReader(os.Stdin)
+		for {
+			char, _, err := inputReader.ReadRune()
+			if err != nil {
+				break
+			}
+			input <- char
+		}
 	}
 	close(input)
 
