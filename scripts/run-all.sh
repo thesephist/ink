42c21ee797c91d6fbb26267759df55feacb8156d
Linus Lee <linus@thesephist.com>
2019-06-23 02:52:45 -0700

    Implement a run-all script for testing

---
+++ scripts/run-all.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+go run . -input samples/stdlib.ink \
+    -input samples/fizzbuzz.ink \
+    -input samples/basic.ink \
+    -input samples/dict.ink \
+    -input samples/fib.ink \
+    -input samples/orderofops.ink \
